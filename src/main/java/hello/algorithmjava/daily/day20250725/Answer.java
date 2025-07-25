fad1b53aab89a2f0d0ca0e79a97261c4c4216069
안동환 <ddh1713flrwls1713@gmail.com>
2025-07-25 17:38:58 +0900

    20250725: 문자열 겹쳐쓰기

---
+++ src/main/java/hello/algorithmjava/daily/day20250725/Answer.java
@@ -0,0 +1,29 @@
+package hello.algorithmjava.daily.day20250725;
+
+public class Answer {
+
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+
+        String start = my_string.substring(0, s);
+        String end = my_string.substring(s + overwrite_string.length());
+
+        answer = start + overwrite_string + end;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Answer answer = new Answer();
+
+        // ex 1
+        String result1 = answer.solution("He11oWor1d", "lloWorl", 2);
+        System.out.println(result1); // HelloWorld
+
+        // ex 2
+        String result2 = answer.solution("Program29b8UYP", "merS123", 7);
+        System.out.println(result2); // HelloWorld
+
+    }
+}
---
+++ src/main/java/hello/algorithmjava/daily/day20250725/Solution.java
@@ -0,0 +1,11 @@
+package hello.algorithmjava.daily.day20250725;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+}
