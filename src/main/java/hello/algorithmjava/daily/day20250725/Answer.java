4bedd918d2e217a9ef2397a8e614a57538b15755
안동환 <ddh1713flrwls1713@gmail.com>
2025-07-25 17:41:25 +0900

    20250725: 문자열 겹쳐쓰기 (updated)

--- src/main/java/hello/algorithmjava/daily/day20250725/Answer.java
+++ src/main/java/hello/algorithmjava/daily/day20250725/Answer.java
@@ -3,14 +3,11 @@ package hello.algorithmjava.daily.day20250725;
 public class Answer {
 
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
 
         String start = my_string.substring(0, s);
         String end = my_string.substring(s + overwrite_string.length());
 
-        answer = start + overwrite_string + end;
-
-        return answer;
+        return start + overwrite_string + end;
     }
 
     public static void main(String[] args) {
