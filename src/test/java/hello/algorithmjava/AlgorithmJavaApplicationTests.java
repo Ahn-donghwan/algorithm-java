b9e813a70fcbfadb0d826889b84158ec65ae83b9
안동환 <ddh1713flrwls1713@gmail.com>
2025-07-25 17:00:56 +0900

    first commit

---
+++ src/test/java/hello/algorithmjava/AlgorithmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.algorithmjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
