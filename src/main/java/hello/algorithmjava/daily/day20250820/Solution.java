3160154941af989dc20a6c24343021bf3e9aa993
안동환 <ddh1713flrwls1713@gmail.com>
2025-08-21 09:44:00 +0900

    20250821: 특정 문자 제거하기

---
+++ src/main/java/hello/algorithmjava/daily/day20250820/Solution.java
@@ -0,0 +1,9 @@
+package hello.algorithmjava.daily.day20250820;
+
+class Solution {
+
+    // 특정 문자 제거하기
+    public String solution(String my_string, String letter) {
+        return my_string.replace(letter, "");
+    }
+}
