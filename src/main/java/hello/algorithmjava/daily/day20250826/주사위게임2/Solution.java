034fb672a250f8d8472b4501bdbedba69ab12f8d
안동환 <ddh1713flrwls1713@gmail.com>
2025-08-26 11:53:19 +0900

    20250826: 주사위 게임 2

---
+++ src/main/java/hello/algorithmjava/daily/day20250826/주사위게임2/Solution.java
@@ -0,0 +1,17 @@
+package hello.algorithmjava.daily.day20250826.주사위게임2;
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        if(a == b && b == c) {
+            answer = (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)));
+        } else if (a == b || b == c || c == a) {
+            answer = (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)));
+        } else {
+            answer = a + b + c;
+        }
+
+        return answer;
+    }
+}
