b0cf96cc89f347348abc608723a2d8a16fe4ebea
안동환 <ddh1713flrwls1713@gmail.com>
2025-08-29 10:17:12 +0900

    20250829: 수열과 구간 쿼리 3

---
+++ src/main/java/hello/algorithmjava/daily/수열과구간쿼리3/Solution.java
@@ -0,0 +1,17 @@
+package hello.algorithmjava.daily.수열과구간쿼리3;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+
+            int tmp = arr[i]; // 서로 스왑할 임시 공간 마련
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+
+        return arr;
+    }
+}
