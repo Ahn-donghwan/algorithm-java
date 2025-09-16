e3cd24e6f1edd5cb4a03e6e72dee2eef93d255be
안동환 <ddh1713flrwls1713@gmail.com>
2025-09-16 09:59:46 +0900

    20250916: 첫 번째로 나오는 음수

---
+++ src/main/java/hello/algorithmjava/daily/day20250916/첫번째로나오는음수/Solution.java
@@ -0,0 +1,16 @@
+package hello.algorithmjava.daily.day20250916.첫번째로나오는음수;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = -1;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
