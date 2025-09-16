1c265610f818d451764b4d3947ee2f5573705697
안동환 <ddh1713flrwls1713@gmail.com>
2025-09-16 20:22:25 +0900

    20250916: n 번째 원소부터

---
+++ src/main/java/hello/algorithmjava/daily/day20250916/n번째원소부터/Solution.java
@@ -0,0 +1,9 @@
+package hello.algorithmjava.daily.day20250916.n번째원소부터;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return  Arrays.copyOfRange(num_list, n - 1, num_list.length);
+    }
+}
