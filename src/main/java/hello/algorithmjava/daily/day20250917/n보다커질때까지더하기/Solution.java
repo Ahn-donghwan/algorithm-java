2ee7f73ed2dad4e1918dc30b509229b87b511df0
안동환 <ddh1713flrwls1713@gmail.com>
2025-09-17 16:36:54 +0900

    20250917: n보다 커질 때까지 더하기

---
+++ src/main/java/hello/algorithmjava/daily/day20250917/n보다커질때까지더하기/Solution.java
@@ -0,0 +1,16 @@
+package hello.algorithmjava.daily.day20250917.n보다커질때까지더하기;
+
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+            if (answer > n) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
