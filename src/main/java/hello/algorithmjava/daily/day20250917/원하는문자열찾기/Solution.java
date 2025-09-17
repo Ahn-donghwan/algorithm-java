1bfaed0e3ffdf945b57635cacaf5b2d22c487a11
안동환 <ddh1713flrwls1713@gmail.com>
2025-09-17 21:14:44 +0900

    20250917: 원하는 문자열 찾기

---
+++ src/main/java/hello/algorithmjava/daily/day20250917/원하는문자열찾기/Solution.java
@@ -0,0 +1,7 @@
+package hello.algorithmjava.daily.day20250917.원하는문자열찾기;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
+    }
+}
