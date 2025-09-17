74c4fcd0347b0c1915a72a1e7480d2aae0b33ad5
안동환 <ddh1713flrwls1713@gmail.com>
2025-09-17 16:32:50 +0900

    20250917: 할 일 목록

---
+++ src/main/java/hello/algorithmjava/daily/day20250917/할일목록/Solution.java
@@ -0,0 +1,19 @@
+package hello.algorithmjava.daily.day20250917.할일목록;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                list.add(todo_list[i]);
+            }
+        }
+
+        return list.stream().toArray(String[]::new);
+    }
+}
