e2c64214eea8ef99c50f0ebf7db047c73b95b4b3
안동환 <ddh1713flrwls1713@gmail.com>
2025-10-01 13:23:37 +0900

    20251001: 뒤에서 5등까지

---
+++ src/main/java/hello/algorithmjava/daily/day20251001/뒤에서5등까지/Solution.java
@@ -0,0 +1,17 @@
+package hello.algorithmjava.daily.day20251001.뒤에서5등까지;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+
+        Arrays.sort(num_list);
+
+        for (int i = 0; i < 5; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
